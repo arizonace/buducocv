8616527575965b1876c0ccddf01f8c44e307af0e
arizonace <git@butterflydust.com>
2024-11-24 20:55:58 -0500

    Added csvfields.py with the ability to choose and reorder columns. Dropped employment-short-para.csv and employment-no-para.csv as they are derivable by picking fields from employment.csv. Factored OutputWrapper.py for use in both Python scripts. Improvements to csv2json.py. Added the ability to specify the dict_key by index. This is particularly useful as I can now create dictionaries for all CSV files in a for loop without specifying the key name for each one.

---
+++ data/OutputWrapper.py
@@ -0,0 +1,23 @@
+import sys
+class OutputWrapper:
+    def __init__(self, filename=None, encoding="utf-8"):
+        if filename:
+            self.output = open(filename, mode='w', encoding=encoding)
+        else:
+            self.output = sys.stdout
+
+    def write(self, message):
+        self.output.write(message)
+
+    def writelines(self, lines):
+        self.output.writelines(lines)
+
+    def close(self):
+        if self.output is not sys.stdout:
+            self.output.close()
+            
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
